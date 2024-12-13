922bdf02452165c0ef529c88169648df62ae7114
Max Vigueras <mvt.2000@hotmail.com>
2024-12-12 23:41:22 -0600

    Chat: Message, Answer & Typing bubbles added

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,1 +1,4 @@
-export * from './SidebarMenuItem'
+export * from './sidebar/SidebarMenuItem'
+export * from './chat-bubbles/GptMessage'
+export * from './chat-bubbles/MyMessage'
+export * from './loaders/TypingLoader'
--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,5 +1,19 @@
+import { GptMessage, MyMessage, TypingLoader } from "../../components"
+
 export const OrthographyPage = () => {
   return (
-    <div>OrthographyPage</div>
+    <div className="chat-container">
+      <div className="chat-messages">
+      <div className="grid grid-cols-12 gap-y-2">
+        {/* Bienvenida  */}
+        <GptMessage text="Hola puedes escribir tu texto y te ayudo a corregir la ortografía"/>
+
+        <MyMessage text="Hola mundo"/>
+        
+          <TypingLoader className="fade-in"/>
+        
+      </div>
+      </div>
+    </div>
   )
 }
