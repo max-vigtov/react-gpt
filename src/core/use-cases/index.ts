2a4982c81626b7ec715f5cd2b57242cdd3c2371a
Max Vigueras <mvt.2000@hotmail.com>
2025-01-04 17:30:16 -0600

    Text Audio: Processing & play audio response

--- src/core/use-cases/index.ts
+++ src/core/use-cases/index.ts
@@ -3,3 +3,4 @@ export * from './pro-cons.use-case';
 export * from './pro-cons-stream.use-case';
 export * from './pro-cons-stream-generator.use-case';
 export * from './translate-text.use-case';
+export * from './text-to-audio.use-case';
